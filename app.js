37e5e4c1b22a68fd045721dd5fe29d7b298d2c64
ptrompeter <patrick.trompeter@gmail.com>
2019-08-14 17:31:53 -0700

    refactored canvas element to create hitbox sensitive context for two boxes.

--- app.js
+++ app.js
@@ -63,18 +63,49 @@ board.addEdge("c2", "d2");
 
 board.printEdges();
 
+//Starting work on a canvas element to plug in here.  A re-org will be needed.
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d')
+
+//working on making clickable boxes.  Step one seems like a list of boxes
+const boxes = [
+  {
+    name: "boxOne",
+    width: 50,
+    height: 100,
+    color: "black",
+    x: 10,
+    y: 10
+  },
+  {
+    name: "boxTwo",
+    width: 100,
+    height: 50,
+    color: "black",
+    x: 210,
+    y: 210
+  }
+];
+
 function startGame() {
   myGameArea.start();
   // let myGamePiece = new component(30, 30, "red", 10, 120);
-  let boxOne = makeBoardRegion(50, 100, "black", 10, 10);
-  let boxTwo = makeBoardRegion(100, 50, "black", 210, 210);
+  // let boxOne = makeBoardRegion(50, 100, "black", 10, 10, "boxOne");
+  // let boxTwo = makeBoardRegion(100, 50, "black", 210, 210, "boxTwo");
+
+  //substituting fixed boxes for iterating over my list.
+  for (var box of boxes){
+    makeBoardRegion(box.width, box.height, box.color, box.x, box.y, box.name);
+  }
 }
 
-function makeBoardRegion(width, height, color, x, y) {
+//Make a function to draw the outlines of empty rectangles.
+function makeBoardRegion(width, height, color, x, y, hitId) {
   const ctx = myGameArea.context;
   ctx.strokeRect(x, y, width, height);
 }
 
+//this function draws filled rectangles.
 function component(width, height, color, x, y) {
   this.width = width;
   this.height = height;
@@ -87,13 +118,35 @@ function component(width, height, color, x, y) {
   ctx.fillRect(this.x, this.y, this.width, this.height);
 }
 
+//myGameArea is the variable that holds the context for the canvas element.
 var myGameArea = {
-  canvas : document.createElement("canvas"),
+  canvas : document.getElementById("canvas"),
   start : function() {
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
     this.context = this.canvas.getContext("2d");
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    // document.body.insertBefore(this.canvas, document.body.childNodes[0]);
   }
 }
+
+//adding a function to detect intersection between a click and my boxes
+function isIntersect(point, box) {
+  if (box.x <= point.x && point.x <= (box.x + box.width) && box.y <= point.y && point.y <= (box.y + box.height)){
+    return box.name
+  }
+  else {
+    return false
+  }
+}
+//adding a canvas event listener that can respond to clicking on boxes
+canvas.addEventListener('click', (e) => {
+  const point = {
+    x: e.clientX,
+    y: e.clientY
+  };
+  for (var box of boxes) {
+    console.log(isIntersect(point, box));
+  }
+})
+
 startGame();
