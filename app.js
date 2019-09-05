108442a638ac2e3fc044e8726c811856239727dd
ptrompeter <patrick.trompeter@gmail.com>
2019-09-05 10:32:55 -0700

    game and rules links now work, fading in and out (game and rules summary) and (verbose rules) on click.

---
+++ app.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// const row1 = document.getElementById("row1");
+// const row2 = document.getElementById("row2");
+$( document ).ready(function() {
+  const row1 = $('#row1');
+  const row2 = $('#row2');
+  const gameLink = document.getElementById("game-link");
+  const rulesLink = document.getElementById("rules-link");
+
+  rulesLink.addEventListener('click', (e) => {
+    row1.fadeOut(400, function(){
+      row2.fadeIn(400);
+    });
+  });
+
+  gameLink.addEventListener('click', (e) => {
+    row2.fadeOut(400, function(){
+      row1.fadeIn(400);
+      refreshBoard(board);
+    });
+  });
+  row2.hide();
+  // row2.css("opacity", 1);
+});
